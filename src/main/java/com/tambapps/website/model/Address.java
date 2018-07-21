3b71594179702b103c9c58f6d23b2a671b3bbfa5
Nelson <tambue@hotmail.fr>
2018-07-21 13:00:04 +0200

    added javax constraints for validation

--- src/main/java/com/tambapps/website/model/Address.java
+++ src/main/java/com/tambapps/website/model/Address.java
@@ -1,14 +1,20 @@
 package com.tambapps.website.model;
 
 import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
 
 @Data
+@RequiredArgsConstructor
 @Entity
 @Table(name = "address")
 public class Address {
@@ -16,10 +22,26 @@ public class Address {
   @GeneratedValue
   @Column(name = "address_id")
   private Long id;
+  @NonNull
+  @Pattern(regexp = User.ALPHABETIC_REGEX)
   private String state;
   @Column(name = "postal_code")
-  private int postalCode;
+  @NonNull
+  @Positive
+  private Integer postalCode;
+  @NonNull
+  @Pattern(regexp = User.ALPHABETIC_REGEX)
   private String city;
+  @NonNull
+  @NotBlank
   private String street; //ex: 2 rue blabla
 
+  public static Address copy(Address address) throws NullPointerException {
+    return new Address(address.state, address.postalCode, address.city, address.street);
+  }
+
+  //constructor to check npo when retrieving address from json
+  private Address(Address address) {
+    this(address.state, address.postalCode, address.city, address.street);
+  }
 }
--- src/main/java/com/tambapps/website/model/User.java
+++ src/main/java/com/tambapps/website/model/User.java
@@ -1,29 +1,41 @@
 package com.tambapps.website.model;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import javax.validation.constraints.Pattern;
 
 @Data
 @Entity
-@RequiredArgsConstructor(staticName = "withId")
-@NoArgsConstructor
+@RequiredArgsConstructor
 @Inheritance(strategy = InheritanceType.JOINED)
 @Table(name = "user", uniqueConstraints = { @UniqueConstraint(columnNames = {"email"})}) //TODO to implement in sql script
 public class User {
 
+  //TODO add '-' and character like 'é', 'è', ...
+  static final String ALPHABETIC_REGEX = "[a-zA-Z]+";
+
   @Id
   @GeneratedValue
-  @NonNull
   @Column(name = "user_id")
-  private Long id;
-  private String email;
-  private String password;
-  private String name;
+  protected Long id;
+  @NonNull
+  @Email
+  protected String email;
+  @NonNull
+  @NotBlank
+  protected String password;
+  @NonNull
+  @Pattern(regexp = ALPHABETIC_REGEX)
+  protected String name;
   @Column(name = "last_name")
-  private String lastName;
+  @NonNull
+  @Pattern(regexp = ALPHABETIC_REGEX)
+  protected String lastName;
+
+  public User(){}
 
 }
