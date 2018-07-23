3c7db5775734b17a4bbc747664d8279bb7b3d45b
Fonkoua Tambue Nelson <nfonkoua@weborama.com>
2018-07-23 17:54:00 +0200

    started implement Pizza.js

--- app-client/src/model/Pizza.js
+++ app-client/src/model/Pizza.js
@@ -2,5 +2,22 @@ import React, { Component } from 'react';
 import './Pizza.css';
 
 class Pizza extends Component {
-    //TODO
+
+    render() {
+        return (
+            <div className="pizza-content">
+                <div className="pizza-header">
+                    <div className="pizza-description">
+                        {this.props.pizza.description}
+                    </div>
+                </div>
+                <div className="poll-choices">
+                    <RadioGroup
+                        className="pizza-ingredients">
+                        { ingredients }
+                    </RadioGroup>
+                </div>
+            </div>
+        );
+    }
 }
